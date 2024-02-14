d22fd9cdf80cc86c946a56992288c7460e674154
Petr Blue <petrpm88@email.cz>
2024-02-14 20:47:49 +0100

    Created the OldStyleApp.

---
+++ OldStyleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OldStyleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
