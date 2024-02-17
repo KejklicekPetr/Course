9a8bc223fee626112a97a981beff91135dcd0756
Petr Blue <petrpm88@email.cz>
2024-02-17 12:47:37 +0100

    Episode 29

---
+++ IntParsing/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IntParsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+            int myInt = int.Parse(myString);
+            int mySecondInt = int.Parse(mySecondString);
+            int result = myInt + mySecondInt;
+            string badResult = myString + mySecondString;
+            Console.WriteLine(result);
+            Console.WriteLine("Bad: " + badResult);
+        }
+    }
+}
