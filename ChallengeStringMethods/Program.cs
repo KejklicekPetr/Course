a97a35ef9afb7f4e22e510d95b39d63f94cbb189
Petr Blue <petrpm88@email.cz>
2024-02-17 13:53:36 +0100

    Challenge #1 - Episode 33

---
+++ ChallengeStringMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ChallengeStringMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Hello user. Please enter your name and press enter: ");
+            name = Console.ReadLine();
+            Console.WriteLine($"Your name is: {name}.");
+            Console.WriteLine($"Your name in uppercase is: {name.ToUpper()}.");
+            Console.WriteLine($"Your name in lowercase is: {name.ToLower()}.");
+            Console.WriteLine($"Your name wo whitespace: {name.Replace(" ", "")}.");
+            Console.WriteLine($"Your name wo your first character: {name.Substring(1)}.");
+        }
+    }
+}
