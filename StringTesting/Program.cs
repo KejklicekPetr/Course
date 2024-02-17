e72d7d755b2aad566e2be5d6bb9f918d12672ebd
Petr Blue <petrpm88@email.cz>
2024-02-17 11:46:30 +0100

    Episode 23

---
+++ StringTesting/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "blue";
+            string msg = "My name is " + myName;
+            string caps = msg.ToUpper();
+            string lower = msg.ToLower();
+            Console.WriteLine(msg);
+            Console.WriteLine(caps);
+            Console.WriteLine(lower);
+            Console.ReadKey();
+        }
+    }
+}
