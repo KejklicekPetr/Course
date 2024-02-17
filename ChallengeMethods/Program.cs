f3e48e89fc578f636b9ecf578048fa4346afff35
Petr Blue <petrpm88@email.cz>
2024-02-17 15:50:41 +0100

    Challenge #4 - Episode 47

---
+++ ChallengeMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChallengeMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "sqrt(-1)";
+            string friend2 = "Dino";
+            string friend3 = "my imaginary friend 2";
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
+        }
+        static void GreetFriend(string name)
+        {
+            Console.WriteLine("Hi " + name + ", my friend!");
+        }
+    }
+}
