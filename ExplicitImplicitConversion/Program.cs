913d98047e7b17b4e20d9b059ded0fd28f605f97
Petr Blue <petrpm88@email.cz>
2024-02-17 12:43:00 +0100

    Episode 28

---
+++ ExplicitImplicitConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExplicitImplicitConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 69420;
+            long bigNum = num; // Implicit conversion
+
+            float myFloat = 69.42f;
+            int myInt = (int)myFloat; // Explicit conversion
+            double myDouble = myFloat; // Implicit conversion
+
+            string myString = myDouble.ToString(); // Explicit conversion
+            string myString2 = myInt.ToString(); // Explicit conversion
+
+            bool sunShining = true;
+            string myBoolString = sunShining.ToString(); // Explicit conversion
+
+            Console.WriteLine("Explicit is just casting. (int); (float); (double); .ToString()");
+            Console.WriteLine("Implicit is just auto conversion, wo data loss. Int => Long; Float => Double");
+        }
+    }
+}
