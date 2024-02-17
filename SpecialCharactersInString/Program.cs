fc7d849247860b331d3cd6759ee40048b47baeb8
Petr Blue <petrpm88@email.cz>
2024-02-17 13:36:20 +0100

    Episode 32

---
+++ SpecialCharactersInString/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SpecialCharactersInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "this is a string with a \"quote\" in it";
+            string s2 = "this is a string with a \\backslash\\ in it";
+            string s3 = "this is a string with a \nnewline in it";
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
